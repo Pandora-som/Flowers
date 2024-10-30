bb6cd366a6b6ae8f77309f836d1a9f31462b4063
Mari <testirovsik0005@mail.ru>
2024-10-30 18:17:58 +0300

    перелистывание страниц и курсор

--- catalogPages.js
+++ catalogPages.js
@@ -1,11 +1,18 @@
 const nextButton = document.getElementById('strelka-right');
 const prevButton = document.getElementById('strelka-left');
 const slides = Array.from(document.getElementsByClassName('catalog-part'));
+const pages = Array.from(document.querySelectorAll('.catalog-nav p'));
 const lenSlider = slides.length;
 let currSlide = 0;
 
 prevButton.addEventListener('click', showPrevious);
 nextButton.addEventListener('click', showNext);
+
+pages.forEach((page, index) => {
+    page.addEventListener('click', function () {
+        selectPage(index);
+    });
+});
 
 function showPrevious() {
     currSlide = (currSlide - 1 + lenSlider) % lenSlider;
@@ -17,6 +24,11 @@ function showNext() {
     updateSlider();
 }
 
+function selectPage(index) {
+    currSlide = index;
+    updateSlider();
+}
+
 function updateSlider() {
     slides.forEach((slide, index) => {
         if (index === currSlide) {
@@ -25,6 +37,31 @@ function updateSlider() {
             slide.style.display = 'none';
         }
     });
-}
+    if (currSlide == 0) {
+        pages[currSlide].style.textDecoration = "underline";
+        pages[currSlide].style.color = "#6AAD46";
+        for (i = 1; i < lenSlider; i++) {
+            pages[i].style.textDecoration = "none";
+            pages[i].style.color = "#000";
+        }
+    } else if (currSlide == 1) {
+        pages[currSlide].style.textDecoration = "underline";
+        pages[currSlide].style.color = "#6AAD46";
 
+        pages[0].style.textDecoration = "none";
+        pages[0].style.color = "#000";
+
+        pages[2].style.textDecoration = "none";
+        pages[2].style.color = "#000";
+    } else {
+        pages[currSlide].style.textDecoration = "underline";
+        pages[currSlide].style.color = "#6AAD46";
+        
+        pages[0].style.textDecoration = "none";
+        pages[0].style.color = "#000";
+
+        pages[1].style.textDecoration = "none";
+        pages[1].style.color = "#000";
+    }
+}
 updateSlider();
